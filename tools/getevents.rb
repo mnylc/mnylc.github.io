c72814c5205bc54ce5ef5b2e1fab479349a8ba0c
jmignault <john.mignault@gmail.com>
2017-03-21 14:10:53 -0400

    Make getevents script take a cmdline arg

--- tools/getevents.rb
+++ tools/getevents.rb
@@ -7,6 +7,8 @@ require 'JSON'
 require 'yaml'
 require 'byebug'
 
+token = ARGV[0]
+
 fname = 'events.yml'
 eventfile = File.open(fname, 'w')
 
@@ -14,7 +16,7 @@ t = Time.new.xmlschema
 tt = t.to_s[0..-7] + 'Z'
 
 uri_str = 'https://www.eventbriteapi.com/v3/events/search/?organizer.id=8929654067&start_date.range_start='
-uri_str += tt + '&token=MUEJI37ZIE226V4SSGIW'
+uri_str += tt + '&token=' + token
 uri = URI(uri_str)
 
 resp = Net::HTTP.get(uri)
