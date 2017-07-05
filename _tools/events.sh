4f191ee9451a3062382c878bea0f4b5760e85045
jmignault <john.mignault@gmail.com>
2017-07-05 11:46:09 -0400

    Ensure rbenv is on PATH before calling it

--- _tools/events.sh
+++ _tools/events.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+export RBENV_ROOT="${HOME}/.rbenv"
+if [ -d "${RBENV_ROOT}" ]; then
+    export PATH="${RBENV_ROOT}/bin:${PATH}"
+    eval "$(rbenv init -)"
+fi
 rbenv local 2.3.1
 cd ~/Documents/git/mnylc.github.io/_tools
 source .event_tokens.sh
--- _tools/jobs.sh
+++ _tools/jobs.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+export RBENV_ROOT="${HOME}/.rbenv"
+if [ -d "${RBENV_ROOT}" ]; then
+    export PATH="${RBENV_ROOT}/bin:${PATH}"
+    eval "$(rbenv init -)"
+fi
 rbenv local 2.3.1
 cd ~/Documents/git/mnylc.github.io/_tools
 git pull origin master
