2c149667dbf70df6dc539c5f4c233f150200f14b
jmignault <john.mignault@gmail.com>
2018-11-01 07:09:36 -0400

    Tweaks to makegrants.rb
    Clean up logic in thumbnail
    Tweak rubocop warnings
    Save corrected to_html in string
    Clarify comments

--- _tools/makegrants.rb
+++ _tools/makegrants.rb
@@ -4,15 +4,15 @@
 # script reads a CSV file and prints out anchors corresponding to
 # grant years followed by a div per corresponding project. Each div
 # consists of a div containing a thumbnail if present which is linked
-# to the institution's collection, also if present, and an information
-# div containing a field for the institution, the collection (also
-# linked to the collection if the link is available), and a
-# description of the collection. The output is inserted into the file
-# digitization-grants.md, between the line of links to the anchors and
-# the Advisory Council section. Immediate TODOs would be to automate
-# the deletion and insertion of the HTML (perhaps by inserting
-# delimiters into the file) and to download the associated spreadsheet
-# automatically as we do with the jobs spreadsheet.
+# to the institution's collection, if a link is available, and an
+# information div containing a field for the institution, the
+# collection (also linked to the collection if the link is available),
+# and a description of the collection. The output is inserted into the
+# file digitization-grants.md, between the line of links to the year-
+# based anchors and the Advisory Council section. Immediate TODOs
+# would be to automate the deletion and insertion of the HTML (perhaps
+# by using a template with a delimiter) and to download the associated
+# spreadsheet automatically as we do with the jobs spreadsheet.
 
 require 'csv'
 require 'nokogiri'
@@ -21,6 +21,7 @@ require 'nokogiri'
 grant_years = {}
 
 # Read each row of the CSV to build project's div
+# rubocop:disable BlockLength
 CSV.foreach(ARGV[0]) do |row|
   # skip the header
   next if row[0].eql?('pid')
@@ -40,16 +41,16 @@ CSV.foreach(ARGV[0]) do |row|
       if tn
         doc.div(class: 'thumb') do
           # if there's a link
+          i = doc.img(src: tn)
           if row[4]
             doc.a(href: row[4]) do
-              i = doc.img(src: tn)
-              # if there's an alt attribute
-              i['alt'] = row[6] if row[6]
+              i
             end
           else
-            i = doc.img(src: tn)
-            i['alt'] = row[6] if row[6]
+            i
           end
+          # if there's an alt attribute
+          i['alt'] = row[6] if row[6]
         end
       end
       # end 'thumb'
@@ -62,11 +63,10 @@ CSV.foreach(ARGV[0]) do |row|
         doc.text(row[2])
         doc.br
         # collection name
-        # if there's a link, wrap the collection name in it
-
         doc.b do
           doc.text('Collection: ')
         end
+        # if there's a link, wrap the collection name in it
         if row[4]
           doc.a(href: row[4]) do
             doc.text(row[3])
@@ -85,14 +85,16 @@ CSV.foreach(ARGV[0]) do |row|
       # end 'info'
     end
     # end 'grant'
-    # add a break tag for space
   end
+  # rubocop:enable
 
+  # fix Nokogiri open <br> tags
+  fixed_br = @bob.to_html.gsub('<br>', '<br/>')
   # push the entry onto the array whose key is the grant year.
   if grant_years.key?(row[1])
-    grant_years[row[1]].push(@bob.to_html.gsub('<br>', '<br/>'))
+    grant_years[row[1]].push(fixed_br)
   else
-    grant_years[row[1]] = [@bob.to_html.gsub('<br>', '<br/>')]
+    grant_years[row[1]] = [fixed_br]
   end
 end
 
